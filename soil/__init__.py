e8988015e242300a569383aaf0d6c5e30b4ef637
J. Fernando Sánchez <balkian@gmail.com>
2017-10-05 16:20:33 +0200

    Add more options to the command line

--- soil/__init__.py
+++ soil/__init__.py
@@ -2,7 +2,7 @@ import importlib
 import sys
 import os
 
-__version__ = "0.9.3"
+__version__ = "0.9.4"
 
 try:
     basestring
@@ -27,6 +27,10 @@ def main():
                         help='python module containing the simulation configuration.')
     parser.add_argument('--module', '-m', type=str,
                         help='file containing the code of any custom agents.')
+    parser.add_argument('--dry-run', '--dry', action='store_true',
+                        help='Do not store the results of the simulation.')
+    parser.add_argument('--output', '-o', type=str,
+                        help='folder to write results to. It defaults to the current directory.')
 
     args = parser.parse_args()
 
@@ -34,8 +38,8 @@ def main():
         sys.path.append(os.getcwd())
         importlib.import_module(args.module)
 
-    print('Loading config file: {}'.format(args.file))
-    simulation.run_from_config(args.file)
+    print('Loading config file: {}'.format(args.file, args.output))
+    simulation.run_from_config(args.file, dump=(not args.dry_run), results_dir=args.output)
 
 
 if __name__ == '__main__':
